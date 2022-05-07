22dce49e3ab0bca428033ee2be140cf56c9c3753
paulmy <paulmul85@gmail.com>
2022-05-07 14:56:42 +0700

    Initial commit

---
+++ app/src/main/java/com/clean/arch/mvvm/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.clean.arch.mvvm.utils
+
+const val API_KEY = "2e774b038b2dc15a1db7397f1b6b63a7"
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val IMAGE_URL = "https://themoviedb.org/t/p/w300"
+const val IMAGE_ORIGINAL_URL = "https://themoviedb.org/t/p/original"
+const val BACKDROP_URL = "https://themoviedb.org/t/p/w780"
